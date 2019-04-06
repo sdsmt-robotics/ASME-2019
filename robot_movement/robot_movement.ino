b27a518019ccf3cabfc42e10c98225283a89d87e
Jordan Pasker <jordan.pasker@mines.sdsmt.edu>
2019-04-06 07:39:16 -0600

    various changes for competition

--- robot_movement/robot_movement.ino
+++ robot_movement/robot_movement.ino
@@ -9,21 +9,22 @@
  *          use millis()
  *      -add toggle for debug messages
 ******************************************************************************/
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
 #include <SPI.h>
 #include <mcp_can.h>
-
+#include "RoboClaw.h"
 /******************************************************************************
  * Defines
 ******************************************************************************/
 //SPX Victor Motor Controllers
-#define controller_one 4
+#define controller_one 3
 #define controller_two 2
-#define controller_three 3 
+#define controller_three 4 
 #define controller_four 1
+#define controller_arm 5
 
 //Max speed for robot movement
-#define MAX_SPD 500.0
+#define MAX_SPD 375.0
 
 //cs pin for SPI and CAN Bus
 #define spiCSPin 53
@@ -32,8 +33,18 @@
 //motor controllers
 #define rc_lift 0x80
 #define rc_corral 0x81
-#define rc_door_right 0x82
-#define rc_door_left 0x83
+#define rc_door 0x80
+#define rc_door_bottom 0x83
+
+//limit switch Defines
+#define lim_bucket_top 27
+#define lim_door_bottom_left 22
+#define lim_door_top_left 23
+#define lim_corral_right 24
+#define lim_door_top_right 25
+#define lim_door_bottom_right 26
+#define lim_bucket_bottom 29
+
 
 /******************************************************************************
  * Globals
@@ -78,8 +89,19 @@ int current_vals[12];
 int axis = 0;
 
 //Test Time
+int old_time = 0;
 int timer = 0;
 int oldspeed;
+
+//corral / door variables
+bool corral_moving = false;
+bool door_moving = false;
+
+unsigned long millis_current = 0;
+unsigned long millis_door = 0;
+unsigned long millis_corral = 0;
+unsigned long delay_corral = 1500;
+unsigned long delay_door = 1500;
 
 /******************************************************************************
  * Object Declaration
@@ -88,30 +110,34 @@ int oldspeed;
 //create CAN object
 MCP_CAN CAN(spiCSPin);
 
-//create roboclaw object
-RoboClaw rc_driver(&Serial3, 10000);
+//create roboclaw objects
+//RoboClaw rc_driver_door(&Serial3, 10000);
+RoboClaw rc_driver_door(&Serial1, 10000);
+RoboClaw rc_driver_corral(&Serial2, 10000);
+
 
 void setup()
 {
     //pc debug
-    Serial.begin(115200);
+    Serial.begin(9600);
 
-    Serial.println("initializing CAN Bus...");
+    //Serial.println("initializing CAN Bus...");
 
     //set the baudrate and let it know we are using the 8MHz oscilator on the CAN module
     while (CAN_OK != CAN.begin(CAN_1000KBPS, MCP_8MHz))
     {
-        Serial.println("CAN BUS init Failed");
+        //Serial.println("CAN BUS init Failed");
         delay(100);
     }
-    Serial.println("CAN BUS Init OK!");   
+    //Serial.println("CAN BUS Init OK!");   
     
     //xBee communication
     Serial.println("Starting Recieve Code");
-    Serial1.begin(9600);
+    Serial3.begin(9600);
  
     //RoboClaw
-    rc_driver,begin(38400);
+    rc_driver_door.begin(38400);
+    rc_driver_corral.begin(38400);
 }
 
 void check_command()
@@ -121,67 +147,75 @@ void check_command()
     {
         axis = incoming_command[0];
         current_vals[(axis - 97)] = map(int(incoming_command[1]), 0, 200, -500, 500);
-        Serial.print("Joystick ");
-        Serial.print(incoming_command[0], HEX);
-        Serial.print(" ");
-        Serial.print(map(incoming_command[1], 0, 200, -500, 500));
-        Serial.println();
+        //Serial.print("Joystick ");
+        //Serial.print(incoming_command[0], HEX);
+        //Serial.print(" ");
+        //Serial.print(map(incoming_command[1], 0, 200, -500, 500));
+        //Serial.println();
     }
-    else if(incoming_command[0] == 'A')
-    {
-        //Right thumb button
-    }
-    else if(incoming_command[0] == 'B')
+    else if(incoming_command[0] == 'L')
     {
         //right bank, right button
+        //lift stop
+        rc_driver_door.ForwardM1(rc_lift, 0);
+        rc_driver_door.ForwardM2(rc_lift, 0);
+        rc_driver_corral.ForwardM1(rc_lift, 0);
+        rc_driver_corral.ForwardM2(rc_lift, 0);
     }
-    else if(incoming_command[0] == 'C')
-    {
-        //right bank, left button
-    }
-    else if(incoming_command[0] == 'D')
+    else if(incoming_command[0] == 'K')
     {
         //right bank, top button
+        //door open
+        rc_driver_door.ForwardM1(rc_lift, 30);
+        rc_driver_door.ForwardM2(rc_lift, 30);
+
+        door_moving = true;
+
+        millis_door = millis();
+    }
+    else if(incoming_command[0] == 'J')
+    {
+        //right bank, bottom button
+        //lift down
+        rc_driver_door.BackwardM1(rc_lift, 30);
+        rc_driver_door.BackwardM2(rc_lift, 30);
+
+        door_moving = true;
+        
+        millis_door = millis();
     }
     else if(incoming_command[0] == 'E')
     {
-        //right bank, bottom button
+        //left bank, right button
+        //corral stop
+        rc_driver_door.ForwardM1(rc_lift, 0);
+        rc_driver_door.ForwardM2(rc_lift, 0);
+        rc_driver_corral.ForwardM1(rc_lift, 0);
+        rc_driver_corral.ForwardM2(rc_lift, 0);
     }
     else if(incoming_command[0] == 'F')
     {
-        //left thumb button
+        //left bank, top button
+        //corral up
+        rc_driver_corral.ForwardM1(rc_lift, 80);
+        rc_driver_corral.ForwardM2(rc_lift, 80);
+        
+        corral_moving = true;
+        corral_moving = true;
+
+        millis_corral = millis();
     }
     else if(incoming_command[0] == 'G')
     {
         //left bank, bottom button
-    }
-    else if(incoming_command[0] == 'H')
-    {
-        //left bank, left button
-    }
-    else if(incoming_command[0] == 'I')
-    {
-        //left bank, top button
-    }
-    else if(incoming_command[0] == 'J')
-    {
-        //left bank, right button
-    }
-    else if(incoming_command[0] == 'K')
-    {
-        //top bank, 
-    }
-    else if(incoming_command[0] == 'L')
-    {
-        //top bank, 
-    }
-    else if(incoming_command[0] == 'M')
-    {
-        //top bank, 
-    }
-    else if(incoming_command[0] == 'N')
-    {
-        //top bank, 
+        //corral down
+        rc_driver_corral.BackwardM1(rc_lift, 80);
+        rc_driver_corral.BackwardM2(rc_lift, 80);
+
+        corral_moving = true;
+
+        millis_corral = millis();
+        
     }
 }
 
@@ -213,9 +247,9 @@ void byte_encoder(unsigned char data[8], int SPD) //Allows Can Bus to Read speed
 
 void loop()
 {
-    if (Serial1.available() > 0)
+    if (Serial3.available() > 0)
     {
-        incomingByte = Serial1.read();
+        incomingByte = Serial3.read();
         if (incomingByte == 0x58)
         {
             //Serial.println();
@@ -230,23 +264,21 @@ void loop()
             }
             incoming_command[queue_len] = incomingByte;
             queue_len++;
-            //Serial.print(incomingByte, HEX);
-            // Serial.print(" ");
         }
-    //Serial.println(incomingByte, HEX);
+       Serial.println(incomingByte);
     }
 
     verL = SpeedControl(current_vals[1]); //Runs function to create speed components
     //Serial.println(verL);
     horL = SpeedControl(current_vals[2]);
     //Serial.println(horL);
-    rotL = SpeedControl(current_vals[3]);
+    rotL = -(SpeedControl(current_vals[3])/7);
     //Serial.println(rotL);
 
-    FRs = -verL - horL - rotL; //Creates the speeds for each motor.
-    BRs = -verL + horL - rotL;
-    FLs = verL - horL - rotL;
-    BLs = verL + horL - rotL; 
+    FRs = -(-verL - horL - rotL); //Creates the speeds for each motor.
+    BRs = -(-verL + horL - rotL);
+    FLs = -(verL - horL - rotL);
+    BLs = -(verL + horL - rotL); 
 
     if (FLs > 0)
     {
@@ -273,31 +305,38 @@ void loop()
 
     //Outputs the speeds to each motor.
     byte_encoder(dataFL, FLs);
-    CAN.sendMsgBuf(CONTROL | controller_one | 0x01040000, 1, 8, dataFL); //0x01040000 for Victor, 0x02040000 for Talon
-    Serial.print(FLs);
-    Serial.print("  ");
+    CAN.sendMsgBuf(CONTROL | controller_one | 0x01040000, 1, 8, dataFL);
 
     byte_encoder(dataFR, FRs);
-    CAN.sendMsgBuf(CONTROL | controller_two | 0x01040000, 1, 8, dataFR); //0x01040000 for Victor, 0x02040000 for Talon
-    Serial.print(FRs);
-    Serial.print("  ");
+    CAN.sendMsgBuf(CONTROL | controller_two | 0x01040000, 1, 8, dataFR);
 
     byte_encoder(dataBR, BRs);
-    CAN.sendMsgBuf(CONTROL | controller_three | 0x01040000, 1, 8, dataBL); //0x01040000 for Victor, 0x02040000 for Talon
-    Serial.print(BLs);
-    Serial.print("  ");
+    CAN.sendMsgBuf(CONTROL | controller_three | 0x01040000, 1, 8, dataBL);
 
     byte_encoder(dataBL, BLs);
-    CAN.sendMsgBuf(CONTROL | controller_four | 0x01040000, 1, 8, dataBR); //0x01040000 for Victor, 0x02040000 for Talon
-    Serial.print(BRs); 
+    CAN.sendMsgBuf(CONTROL | controller_four | 0x01040000, 1, 8, dataBR);
 
+    //send enable frame
     CAN.sendMsgBuf(ENABLE, 1, 8, enable);
 
-    Serial.print("  ");
-    Serial.print(timer);
-    Serial.print("  ");
-    Serial.println(oldspeed);
+    millis_current = millis();
 
+    if(door_moving && millis_current - millis_door > delay_door)
+    {
+        door_moving = false;
+
+        rc_driver_door.ForwardM1(rc_lift, 0);
+        rc_driver_door.ForwardM2(rc_lift, 0);
+    }
+
+    if(corral_moving && millis_current - millis_corral > delay_corral)
+    {
+        corral_moving = false;
+
+        rc_driver_corral.ForwardM1(rc_lift, 0);
+        rc_driver_corral.ForwardM2(rc_lift, 0);
+    }
+/*
     //Deadman's switch for bot movement
     if (timer == 0)
     {
@@ -314,6 +353,6 @@ void loop()
         }
     timer = 0;
     }
-
+*/
     delay(20);
 }
